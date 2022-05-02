63ff478788c1de7f950adff6fb12a749249c5c5c
TinNguyen <ductin16111@gmail.com>
2022-05-02 20:26:56 +0300

    first compile prob 1656C

--- 1656C_Mod-equal.cpp
+++ 1656C_Mod-equal.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
-
+void sort(long int *a, long int size){
+    long int i, k;
+    for (i=0;i<size;i++){
+		for(int j=i+1;j<size;j++){
+		   if(a[i]>a[j]){
+			   k=a[i];
+			   a[i]=a[j];
+			   a[j]=k;
+		    }
+		}
+	}
+}
 
 int main(void){
    int t;  cin >> t;
@@ -9,8 +20,8 @@ int main(void){
         long int n; cin>>n;
         long int i, j;
         long int *arr = new long int [n];
-        for (i = 0; i < n; ++i) cin >> arr[i];
-
-        
+        for (i = 0; i < n; ++i) {
+            cin >> arr[i];
+        }
     }
 }
