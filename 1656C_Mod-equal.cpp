c9bd0ed6ab85a2c694bd782e048afa50bee2df76
TinNguyen <ductin16111@gmail.com>
2022-05-02 20:03:04 +0300

    add problem 1656C

---
+++ 1656C_Mod-equal.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(void){
+   int t;  cin >> t;
+    while(t--){
+        long int n; cin>>n;
+        long int i, j;
+        long int *arr = new long int [n];
+        for (i = 0; i < n; ++i) cin >> arr[i];
+
+        
+    }
+}
