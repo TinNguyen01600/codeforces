169dd70c594866bc3e964e6c5ae9e7ee8a77e428
TinNguyen <ductin16111@gmail.com>
2022-07-26 14:41:20 +0300

    add solution to problem 1705B

---
+++ 1705B_Mark-the-dust-sweeper.cpp
@@ -0,0 +1,18 @@
+/*
+Since the machine only increases/decreases numbers by 1 in each step
+=> The total steps = sum of all elements in array
+ */
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main(void){
+  int t;  cin >> t;
+  while(t--){
+    long int n; cin>>n;
+    long int i = 0;
+    long int *arr = new long int [n];
+    for (i = 0; i < n; ++i)     cin >> arr[i];
+  }
+}
