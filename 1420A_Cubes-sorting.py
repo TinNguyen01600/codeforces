7850f96912f42a38a2952a34f8bb6c6b667bcad9
TinNguyen <ductin16111@gmail.com>
2022-08-26 00:19:09 +0300

    problem 1420A complete by python

--- 1420A_Cubes-sorting.py
+++ 1420A_Cubes-sorting.py
@@ -1,3 +1,13 @@
+# Wheatley uses bubble sorting.
+# The complexity of this algorithm is n(n-1)/2
+# That is, in the worst case (array in descending order), 
+# it takes n(n-1)/2 steps to re-sort the array
+
+# The limit is (n(n-1)/2 - 1) operations, so if the array is not
+# initially descending, the answer is YES
+# if exists a[i-1] <= a[i] in the array, it is not initially descending
+
+
 t = int(input())
 while (t!=0):
     n = int(input())
@@ -5,17 +15,13 @@ while (t!=0):
     i = 1
     count = 0
     while (i < n):
-        if (a[i-1] > a[i]):
-            temp = a[i-1]
-            a[i-1] = a[i]
-            a[i] = temp
-            i = 0
-            count += 1
+        if (a[i-1] <= a[i]):
+            count = 1
+            break
         i += 1
-    # print(a)
-    # print(count)
-    if count > (n*(n-1)/2 - 1):
-        print("NO")
-    else:
+    if count == 1:
         print("YES")
+    else:
+        print("NO")
     t -= 1
+
