55314c54b2b36c16cfaef13bf2e8f6b9c2ae7596
TinNguyen <ductin16111@gmail.com>
2022-08-25 16:03:32 +0300

    problem 1720A complete

--- 1720A_Burenka-fractions.cpp
+++ 1720A_Burenka-fractions.cpp
@@ -7,41 +7,26 @@
 #include <list>
 #include <unordered_map>
 using namespace std;
- 
-int gcd(long int a, long int b){
-    int t;
-    while (b != 0)
-    {
-        t = b;
-        b = a % b;
-        a = t;
-    }
-    return a;
-}
- 
- 
+
+/*
+    the maximum claps needed is 2 because we can change 2 num and den by 2 claps
+    for ex, a/b and c/d is not equal.
+    change a to a * (c*b), change b to b * (d*a), a/b becomes acb/bda = c/d
+
+    if (((a*d) % (c*b) == 0) or ((c*b) % (a*d) == 0)), 1 clap is needed
+    for ex 4/3 and 2/9.
+    change 4/3 to 4/(3*6) = 4/18 = 2/9
+*/
+
 int main(int argc, char const* argv[])
 {
     int t; cin >> t;
     while(t--){
-        long int a,b,c,d;
-        scanf("%ld %ld %ld %ld", &a, &b, &c, &d);
-        if ((float)a/b == (float)c/d)   cout << 0 << endl;
-        else{
-            long int a1 = a, b1 = b, c1 = c, d1 = d;
-            a /= gcd(a1,b1); b /= gcd(a1,b1);
-            c /= gcd(c1,d1); d /= gcd(c1,d1);
-            int i,j,k,l;
-            if (c>=a)   i = c, j = a, k = b, l = d;
-            else    i = a, j = c, k = d, l = b;
-            if (j==0)   cout << 1 << endl;
-            else{
-                float m = (float)i/j;
-                long int  n = m*k;
-                if (n % l == 0)  cout << 1 << endl;
-                else cout << 2 << endl;
-            }
-        }
+        long long a,b,c,d;
+        scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
+        if ((double)a/(double)b == (double)c/(double)d)   cout << 0 << endl;
+        else if (a == 0 || c == 0)  cout << 1 << endl;
+        else if (((a*d) % (c*b) == 0) || ((c*b) % (a*d) == 0))   cout << 1 << endl;
+        else    cout << 2 << endl;
     }
-    
 }
