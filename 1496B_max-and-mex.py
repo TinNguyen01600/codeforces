aa1835bfcca3980cb78a472bc15c1015cc5cc25c
TinNguyen <ductin16111@gmail.com>
2022-08-29 20:04:32 +0300

    problem 1496B completed by python

--- 1496B_max-and-mex.py
+++ 1496B_max-and-mex.py
@@ -1,12 +1,11 @@
+# The added element ((max + mex)/2) does not change no matter k.
+
+# If array are natural numbers (0,1,2,3,...), added element 
+# always NEW and equals (max+1) 
+# => number of disticnt elements increases k after k operations
+
 from math import *
- 
-def count_distict(a):
-    result = []
-    for i in a:
-        if i not in result:
-            result.append(i)
-    return len(result)
- 
+
 t = int(input())
 while (t!=0):
     a = list(map(int, input().split()))
@@ -14,20 +13,18 @@ while (t!=0):
     k = a[1]
     a = list(map(int, input().split()))
     
-    mex = 0
-    while mex in a:
-        mex += 1
     max = 0
     for i in a:
         if i > max:  max = i 
-    ele = ceil((max+mex)/2)
-    
-    result = count_distict(a)
-    
-    if k == 0: result += 0
-    elif ele > max:
-        result = n+k
+    if max == n-1:
+        n += k
     else:
-        if ele not in a:    result += 1
-    print(result)
+        mex = 0
+        while mex in a:
+            mex += 1
+        ele = ceil((max+mex)/2)
+        if k == 0: n += 0
+        else:
+            if ele not in a:    n += 1
+    print(n)
     t -= 1
